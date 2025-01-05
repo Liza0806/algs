e8a10c5a48e7c81ca405f3dd61b308e2bdd96c26
Liza <fildepiorsta@gmail.com>
2025-01-05 14:54:00 +0200

    bin search rec

--- myCode.js
+++ myCode.js
@@ -12,3 +12,18 @@ console.log(arr)
 }
 let b = [5, 3, 8, 4, 2]; 
 //buubbleSort(b)
+
+const insertionSortt = (arr) => {
+    for (let i = 1; i< arr.length; i++){
+        let current = arr[i];
+        let j = i-1;
+        while (j >= 0 && arr[j]>current){
+          
+            arr[j+1] = arr[j];
+            j--;
+        }
+        arr[j+1] = current;
+    }
+    console.log(arr);
+}
+//insertionSortt(b)
